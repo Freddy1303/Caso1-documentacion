fc8074f40ecc6b458f9cff1dcb75832e13647123
Freddy1303 <flopezparia@gmail.com>
2024-02-16 17:50:33 -0500

    Iconos cambiados

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
         title: '',
         logo: {
           alt: 'Documentación de Chat Buho',
-          src: 'https://buho.la/images/chat/chat-buho.svg',
+          src: 'https://fraternitysororitysvg.com/assets/images/products/Among-Us-Svg.png',
         },
         items: [
           // {
